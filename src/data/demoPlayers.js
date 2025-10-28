a1c0bf1511abedf0d63d678cc806d91c6cfa6927
FlippedOut <flippedout123@gmail.com>
2025-10-28 21:22:28 +1100

    Create demoPlayers.js

---
+++ src/data/demoPlayers.js
@@ -0,0 +1,14 @@
+// src/data/demoPlayers.js
+// Small demo set so the UI renders immediately.
+const demoPlayers = [
+  { id: 'p1',  name: 'Alex M',   gender: 'M', skillBracket: '3.0-3.49', skillRating: 3.2 },
+  { id: 'p2',  name: 'Brooke F', gender: 'F', skillBracket: '3.0-3.49', skillRating: 3.1 },
+  { id: 'p3',  name: 'Chris H',  gender: 'M', skillBracket: '3.0-3.49', skillRating: 3.3 },
+  { id: 'p4',  name: 'Dana S',   gender: 'F', skillBracket: '3.0-3.49', skillRating: 3.0 },
+  { id: 'p5',  name: 'Evan P',   gender: 'M', skillBracket: '3.0-3.49', skillRating: 3.4 },
+  { id: 'p6',  name: 'Faith L',  gender: 'F', skillBracket: '3.0-3.49', skillRating: 3.2 },
+  { id: 'p7',  name: 'Gabe T',   gender: 'M', skillBracket: '3.0-3.49', skillRating: 3.1 },
+  { id: 'p8',  name: 'Holly R',  gender: 'F', skillBracket: '3.0-3.49', skillRating: 3.2 },
+];
+
+export default demoPlayers;
